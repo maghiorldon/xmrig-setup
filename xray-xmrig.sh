c78c50b37e352e811d06a30ba9c635be2c8dc15b
maghiorldon <maggioeldon@my.jccmi.edu>
2025-04-12 11:50:07 +0800

    Update xray-xmrig.sh

--- xray-xmrig.sh
+++ xray-xmrig.sh
@@ -23,7 +23,7 @@ sudo cat > /usr/local/etc/xray/config.json<<EOL
           "show": false,
           "dest": "www.chatgpt.com:443",       // 用來混淆的目標地址，可替換成其他常見網站（或 CDN 背後的站點）
           "xver": 0,
-          "serverNames": [],                // 如果有指定假裝的主機名，可在此填入，如 ["www.bing.com"]
+          "serverNames": ["www.chatgpt.com"],                // 如果有指定假裝的主機名，可在此填入，如 ["www.bing.com"]
           "privateKey": "",  // 你必須生成並放入你的 Reality 私鑰 指令：xray x25519
           "shortIds": ["abcd1234"]          // 一組短 ID，作為流量混淆標識，可自定義（建議使用 8 位英數字串）
         },
