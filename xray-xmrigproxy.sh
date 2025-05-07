1129319fd7cbd9edc31968a6a94910b2bf10cb05
maghiorldon <maggioeldon@my.jccmi.edu>
2025-05-07 08:27:09 +0800

    Update xray-xmrigproxy.sh

--- xray-xmrigproxy.sh
+++ xray-xmrigproxy.sh
@@ -17,57 +17,30 @@ bash <(curl -L https://raw.githubusercontent.com/XTLS/Xray-install/main/install-
 sudo cp /usr/local/etc/xray/config.json /usr/local/etc/xray/config.json.backup
 
 sudo cat > /usr/local/etc/xray/config.json<<EOL
-{
-  "inbounds": [
-    {
-      "port": 443, 
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "7f917c60-7486-46af-816c-a43fe4584be1" 
-            }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "tcp",
-        "security": "reality",
-        "realitySettings": {
-          "show": false,
-          "dest": "www.chatgpt.com:443",       
-          "xver": 0,
-          "serverNames": ["www.chatgpt.com"],              
-          "privateKey": "AKgbYBGdBtH0TT8CY3JXUJM2IJcMN3dcqKHFanPf9EE",  
-          "shortIds": ["abcd1234"]          
-        },
-        "tcpSettings": {
-          "header": {
-            "type": "http",
-            "request": {
-            "version": "1.1",
-            "method": "GET",
-              "path": ["/", "/index.html", "/images/logo.png"],
-              "headers": {
-                "Host": ["www.chatgpt.com", "cdn.jsdelivr.net"] ,  
-                "User-Agent":  [
-"Mozilla/5.0  (Windows NT 10.0; Win64; x64)   AppleWebKit/537.36  (KHTML, like Gecko)   Chrome/122.0.0.0 Safari/537.36"
- ],
-                "Accept-Encoding": ["gzip, deflate"],
-                "Connection": ["keep-alive"],
-                "Pragma": "no-cache",
-                "Cache-Control": "no-cache"
-              }
-            }
+{ 
+    "inbounds": [
+      { 
+            "port": 443, 
+            "protocol": "shadowsocks",
+            "settings": {
+                   "method": "chacha20-ietf-poly1305",   
+                   "password": "abc123", 
+                   "network": "tcp,udp"
+                                }
+         }
+                            ], 
+       "outbounds": [
+        { 
+              "protocol": "freedom", 
+              "settings":
+                       {
+                             "servers": [ 
+                               { 
+                                      "address": "127.0.0.1", 
+                                      "port": 3333
           }
-        }
+        ]
       }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
     }
   ]
 }
@@ -79,7 +52,7 @@ Description=XMRig Proxy Service
 After=network.target
 
 [Service]
-ExecStart=/home/proxy/xmrig-proxy-6.22.0/xmrig-proxy -o xmr-asia1.nanopool.org:10343 --coin monero --bind 0.0.0.0:3333 -m simple -u 43cx2hYimLw9YkAYxLG8Vg2TStTL3r6XmbfDfBiCY9MCViYCCaYpEzr1BUCmZTquQwLpg7Sb1FhrV4qR5EXWwvkgKdSHVLd -p 001 -k --tls
+ExecStart=/home/proxy/xmrig-proxy-6.22.0/xmrig-proxy -o pool.supportxmr.com:9000 --coin monero --bind 0.0.0.0:3333 -m simple -u 43cx2hYimLw9YkAYxLG8Vg2TStTL3r6XmbfDfBiCY9MCViYCCaYpEzr1BUCmZTquQwLpg7Sb1FhrV4qR5EXWwvkgKdSHVLd -p 001 -k --tls
 WorkingDirectory=/home/proxy/xmrig-proxy-6.22.0
 User=root
 Group=root
